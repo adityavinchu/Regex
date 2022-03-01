d7a21a1485bef8014d212dbef354011ad9b0c605
aditya <vinchuaditya@gmail.com>
2022-03-01 12:37:48 +0530

    validation of

---
+++ Regex/Pattern.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace validation
+{
+    public class Pattern
+    {
+        public static string firstname = "^[A-Z]{1}[a-z,A-Z,0-9]{2,}$";
+        public bool Validate(string firstname)
+        {
+            return Regex.IsMatch(firstname, firstname);
+        }
+    }
+}
---
+++ Regex/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace validation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+
+            bool flag = pattern.Validate("fkg7p17kbf");
+            Console.WriteLine(flag);
+            Console.ReadLine();
+        }
+    }
+}
