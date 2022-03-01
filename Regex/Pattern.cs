8832b81ab5de3c2d779ec19228b7c7b130f32a22
aditya <vinchuaditya@gmail.com>
2022-03-01 16:30:49 +0530

    validate mobile number

--- Regex/Pattern.cs
+++ Regex/Pattern.cs
@@ -5,10 +5,10 @@ namespace validation
 {
     public class Pattern
     {
-        public static string email = "^[A-Z0-9a-z]{1,}([.#$^_-][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
-        public bool Validate(string Email)
+        public static string mobNumber = "^[0-9]{2}[ ]{1}[6-9]{1}[0-9]{9}$";
+        public bool Validate(string mobileNumber)
         {
-            return Regex.IsMatch(Email, email);
+            return Regex.IsMatch(mobileNumber,mobNumber);
         }
     }
 }
--- Regex/Program.cs
+++ Regex/Program.cs
@@ -11,7 +11,8 @@ namespace validation
         static void Main(string[] args)
         {
             Pattern pattern = new Pattern();
-            Console.WriteLine("Enter email: ");
+            Console.WriteLine("Enter Mobile Number ");
+            Console.WriteLine("In this Format: COUNTRYCODE <space> MOBILE NUMBER Eg: 91 9957448747");
             string str=Console.ReadLine();
 
             bool flag = pattern.Validate(str);
