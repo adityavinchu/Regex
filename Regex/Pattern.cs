e2bef2b1c129171640bbacb7a758c04bc1ece592
adityavinchu <88549097+adityavinchu@users.noreply.github.com>
2022-03-02 08:09:43 +0530

    Update Pattern.cs

--- Regex/Pattern.cs
+++ Regex/Pattern.cs
@@ -5,7 +5,7 @@ namespace validation
 {
     public class Pattern
     {
-        public static string Lname = "^[A-Z]{1}[a-z,A-Z,0-9]{2,}$";
+        public static string Lname = "^[A-Z]{1}[a-z,A-Z]{2,}$";
         public bool Validate(string lastname)
         {
             return Regex.IsMatch(lastname, Lname);
