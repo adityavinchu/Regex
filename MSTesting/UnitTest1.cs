647092eb04148c8380a4d459141104831953c264
aditya <vinchuaditya@gmail.com>
2022-03-13 18:04:16 +0530

    all rules passed

--- MSTesting/UnitTest1.cs
+++ MSTesting/UnitTest1.cs
@@ -1,18 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using validation;
+
 namespace MSTesting
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GivenCorrectPassword_ShouldReturnvalidPassword()
         {
             Pattern password = new Pattern();
-            Assert.IsTrue(password.Validate("Asjsl@457"));
+            Assert.IsTrue(password.Validate("Asdf@457"));
         }
         [TestMethod]
-        public void TestMethod2()
+        public void GivenWrongPassword_ShouldReturnInvalidPassword()
         {
             Pattern password = new Pattern();
             Assert.IsTrue(password.Validate("457844"));
